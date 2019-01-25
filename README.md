47dc2ac017ff313a26865302342b76282876e624
Brian Fitzpatrick <bfitzpat@redhat.com>
2019-01-25 11:58:25 -0700

    Doing a hard fork of the vscode-yo repository
    Original location: https://github.com/SamVerschueren/vscode-yo
    
    Signed-off-by: Brian Fitzpatrick <bfitzpat@redhat.com>

---
+++ README.md
@@ -0,0 +1,28 @@
+# vscode-yeoman
+
+> Scaffold projects using [Yeoman](http://yeoman.io/).
+
+
+## Usage
+
+Press `F1` and type `yeoman`. Pick one of your generators from the list and answer all the questions.
+
+![](https://github.com/bfitzpat/vscode-yo/raw/master/media/yo.gif)
+
+### Sub-Generator
+
+The plugin also supports sub-generators. If a plugin has multiple generators, a list with all the available
+generators will pop-up. The default generator is marked by a ★.
+
+![](https://github.com/bfitzpat/vscode-yo/raw/master/media/sub-generator.gif)
+
+### Pre-requirements
+* [Node.js](https://nodejs.org)
+* [npm](https://www.npmjs.com) 
+* [Yeoman](http://yeoman.io)
+* A [Yeoman generator](http://yeoman.io/generators/) of choice
+
+## License
+
+MIT © [Sam Verschueren](http://github.com/SamVerschueren)
+MIT © [bfitzpat](http://github.com/bfitzpat)
---
+++ src/extension.ts
@@ -0,0 +1,137 @@
+'use strict';
+
+import {window, workspace, commands, ExtensionContext, QuickPickItem, InputBoxOptions} from 'vscode';
+import EscapeException from './utils/EscapeException';
+import runAsync from './utils/run-async';
+import Yeoman from './yo/yo';
+
+const path = require('path');
+const fs = require('fs');
+const figures = require('figures');
+const opn = require('opn');
+
+export function activate(context: ExtensionContext) {
+	const cwd = workspace.rootPath;
+
+	const disposable = commands.registerCommand('yeoman', () => {
+		if (!cwd) {
+			window.showErrorMessage('Please open a workspace directory first.');
+			return;
+		}
+
+		const yo = new Yeoman({cwd});
+		let main;
+		let sub;
+
+		Promise.resolve(window.showQuickPick(list(yo), { 
+			placeHolder: 'Select one of the available Yeoman generators below.', 
+			ignoreFocusOut: true
+		})).then((generator: any) => {
+				if (generator === undefined) {
+					throw new EscapeException();
+				}
+
+				main = generator.label;
+
+				if (generator.subGenerators.length > 1) {
+					return runSubGenerators(generator.subGenerators);
+				} else {
+					return 'app';
+				}
+			})
+			.then(subGenerator => {
+				if (subGenerator === undefined) {
+					throw new EscapeException();
+				}
+
+				sub = subGenerator;
+
+				return yo.run(`${main}:${sub}`, cwd);
+			})
+			.catch(err => {
+				const regexp = new RegExp('Did not provide required argument (.*?)!', 'i');
+
+				if (err) {
+					const match = err.message.match(regexp);
+
+					if (match) {
+						return `${sub} ${match[1]}?`;
+					}
+				}
+
+				throw err;
+			})
+			.then((question: any) => {
+				return window.showInputBox({prompt: question})
+					.then(input => {
+						if (!input) {
+							throw new EscapeException();
+						}
+
+						return input;
+					});
+			})
+			.then(argument => {
+				return yo.run(`${main}:${sub} ${argument}`, cwd);
+			})
+			.catch(err => {
+				if (!err || err instanceof EscapeException) {
+					return;
+				}
+
+				window.showErrorMessage(err.message || err);
+			});
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+function runSubGenerators(subGenerators: string[]) {
+	const app = `${figures.star} app`;
+	const index = subGenerators.indexOf('app');
+
+	if (index !== -1) {
+		subGenerators.splice(index, 1);
+		subGenerators.unshift(app);
+	}
+
+	return window.showQuickPick(subGenerators)
+		.then(choice => {
+			if (choice === app) {
+				return 'app';
+			}
+
+			return choice;
+		});
+}
+
+function list(yo: Yeoman): Promise<QuickPickItem[]> {
+	return new Promise((resolve, reject) => {
+		setImmediate(() => {
+			yo.getEnvironment().lookup(() => {
+				const generators = yo.getGenerators().map(generator => {
+					return {
+						label: generator.name.replace(/(^|\/)generator\-/i, '$1'),
+						description: generator.description,
+						subGenerators: generator.subGenerators
+					};
+				});
+
+				if (generators.length === 0) {
+					reject();
+
+					window.showInformationMessage('Make sure to install some generators first.', 'more info')
+						.then(choice => {
+							if (choice === 'more info') {
+								opn('http://yeoman.io/learning/');
+							}
+						});
+
+					return;
+				}
+
+				resolve(generators);
+			});
+		});
+	});
+}
