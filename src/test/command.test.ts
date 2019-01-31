840681b744ea1cc34a21450060b1c142357d481c
Brian Fitzpatrick <bfitzpat@redhat.com>
2019-01-31 06:58:30 -0700

    issue #1 - adding initial test
    -- moving typescript dependency from devDependencies to dependencies to
    fix weird issue with vsc prepublish task
    -- adjusting travis config
    -- fixing versions to build vsc package without errors
    -- correcting url in package file
    
    Signed-off-by: Brian Fitzpatrick <bfitzpat@redhat.com>

---
+++ src/test/command.test.ts
@@ -0,0 +1,39 @@
+"use strict";
+
+import * as vscode from "vscode";
+import * as chai from "chai";
+import * as sinonChai from "sinon-chai";
+import * as sinon from "sinon";
+
+const expect = chai.expect;
+chai.use(sinonChai);
+
+describe("vscode/commands", () => {
+	let sandbox: sinon.SinonSandbox;
+	let inputStub: sinon.SinonStub;
+
+	before(() => {
+		sandbox = sinon.createSandbox();
+		inputStub = sandbox.stub(vscode.window, "showInputBox");
+	});
+
+	after(() => {
+		sandbox.restore();
+	});
+
+	describe("Yeoman and Yo", () => {
+		before(() => {
+			inputStub.onFirstCall().returns("goonies");
+		});
+
+		after(() => {
+			inputStub.reset();
+		});
+
+		it("works by entering yeoman or yo", () => {
+			vscode.commands.executeCommand("yeoman.yeoman")
+			vscode.commands.executeCommand("yeoman.yo")
+		});
+	});
+
+});
