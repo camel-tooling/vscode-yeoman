092ac68532f17bc5b2dba1d05187980562a142be
Brian Fitzpatrick <bfitzpat@redhat.com>
2019-09-24 15:27:39 -0600

    tidying up an indent issue and updating package-lock
    Signed-off-by: Brian Fitzpatrick <bfitzpat@redhat.com>

--- src/test/list-generators.test.ts
+++ src/test/list-generators.test.ts
@@ -3,10 +3,9 @@ import { expect } from "chai";
 import listGenerators from "../utils/list-generators";
 
 describe("List generators", () => {
-    it("locally installed generators are available", async () => {
-        let quickPicks = await listGenerators(new Yeoman({cwd: __dirname}));
-        // generator-generator should be visible as it is a dev dependency of this project
-        expect(quickPicks.map(_ => _.label)).to.contain("generator");
-    });
-
-})
+	it("locally installed generators are available", async () => {
+		let quickPicks = await listGenerators(new Yeoman({cwd: __dirname}));
+		// generator-generator should be visible as it is a dev dependency of this project
+		expect(quickPicks.map(_ => _.label)).to.contain("generator");
+	});
+});
